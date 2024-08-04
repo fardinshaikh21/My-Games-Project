86f7a7faf7a20126fefc55979ac8f5c7f649b145
ShaikhFardin786 <fardinshaikh8875@gmail.com>
2024-08-04 20:28:04 +0530

    Add also new game Hungman

---
+++ Hungman_Game.py
@@ -0,0 +1,43 @@
+import random as rd
+import Hungman_Stages as HS
+
+fruits = ["apple","banana","greaps","mango","orange"]
+
+
+fruit = rd.choice(fruits)
+#print("\t",fruit,"\n")
+
+display = [] 
+lives = 6
+
+for i in range(len(fruit)):
+    display += '_'
+    
+print(display,"\n")   
+
+flag = False
+
+while not flag:
+    
+    Guessed_Letter = input("\nEnter Guess Character : ")
+    
+    for position in range(len(fruit)):
+        
+        if fruit[position] == Guessed_Letter:   
+            display[position] = Guessed_Letter
+            print("\nCorrect Choice")
+            
+    print("\n",display)
+    
+    if Guessed_Letter not in fruit:
+         lives -= 1
+         print(HS.Stages[lives])
+         print("\nWrong Answer Your Lives is : ",lives)
+         if lives == 0:
+            flag = True
+            print("\nGame is Over You Lose!")             
+            
+    if '_' not in display:
+        flag = True
+        print("\nYou Win")  
+        
